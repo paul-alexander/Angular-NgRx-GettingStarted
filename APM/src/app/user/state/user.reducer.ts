2e768435638b7bc373da1d6cd1e0501524c50291
Paul Alexander <p_alexander@btinternet.com>
2020-12-03 16:43:41 +0000

    Chapter 4 - Homework - Added MaskUsername to User store

---
+++ APM/src/app/user/state/user.reducer.ts
@@ -0,0 +1,12 @@
+import { createAction, createReducer, on } from '@ngrx/store';
+
+export const userReducer = createReducer(
+  { maskUserName: false },
+  on(createAction('[User] Toggle Mask User Name'), (state) => {
+
+    return {
+      ...state,
+      maskUserName: !state.maskUserName,
+    };
+  })
+);
