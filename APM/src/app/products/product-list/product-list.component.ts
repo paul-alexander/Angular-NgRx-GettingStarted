e9ab461fbaaa41083bcd11304fbc0bdfde7b9f08
Paul Alexander <p_alexander@btinternet.com>
2020-12-04 16:15:20 +0000

    Chapter 6 - Hook up edit and list to use Store instead of service

--- APM/src/app/products/product-list/product-list.component.ts
+++ APM/src/app/products/product-list/product-list.component.ts
@@ -1,14 +1,12 @@
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-
-import { Subscription } from 'rxjs';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 
 //note State is Products defined State that extends App State
-import { getShowProductCode, State } from '../state/product.reducer';
+import { getCurrentProduct, getShowProductCode, State } from '../state/product.reducer';
 
 import * as ProductActions from '../state/product.actions'
 
@@ -17,7 +15,7 @@ import * as ProductActions from '../state/product.actions'
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent implements OnInit {
   pageTitle = 'Products';
   errorMessage: string;
 
@@ -27,7 +25,6 @@ export class ProductListComponent implements OnInit, OnDestroy {
 
   // Used to highlight the selected product in the list
   selectedProduct: Product | null;
-  sub: Subscription;
 
   constructor(
     private store: Store<State>,
@@ -35,9 +32,12 @@ export class ProductListComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    this.sub = this.productService.selectedProductChanges$.subscribe(
-      (currentProduct) => (this.selectedProduct = currentProduct)
-    );
+
+    //TODO - Unsubscribe
+    this.store.select(getCurrentProduct).subscribe(
+      currentProduct => this.selectedProduct = currentProduct
+    )
+
 
     this.productService.getProducts().subscribe({
       next: (products: Product[]) => (this.products = products),
@@ -50,19 +50,16 @@ export class ProductListComponent implements OnInit, OnDestroy {
     );
   }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
-
   checkChanged(): void {
     this.store.dispatch(ProductActions.toggleProductCode());
   }
 
   newProduct(): void {
-    this.productService.changeSelectedProduct(this.productService.newProduct());
+    this.store.dispatch(ProductActions.initializeCurrentProduct());
+
   }
 
   productSelected(product: Product): void {
-    this.productService.changeSelectedProduct(product);
+    this.store.dispatch(ProductActions.setCurrentProduct({product}));
   }
 }
